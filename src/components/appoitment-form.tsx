5c5694455acb6020864580448675b1bba90c15ee
Bernardo Freguglia <beefreguglia@gmail.com>
2025-12-11 16:49:42 -0300

    feature: create input and text-area

--- src/components/appoitment-form.tsx
+++ src/components/appoitment-form.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
+import { Dog, User } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
 import {
   Dialog,
   DialogContent,
@@ -11,13 +14,21 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
-import { Button } from './ui/button';
-import { Form } from './ui/form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
 
 const appointmentFormSchema = z.object({
   tutorName: z.string().min(3, 'O nome do tutor é obrigatório'),
   petName: z.string().min(3, 'O nome do pet é obrigatório'),
-  phone: z.string().min(11, 'O número de telefone é obrigatório'),
+  // phone: z.string().min(11, 'O número de telefone é obrigatório'),
   description: z.string().min(3, 'A descrição é obrigatória'),
 });
 
@@ -28,9 +39,9 @@ export function AppointmentForm() {
     resolver: zodResolver(appointmentFormSchema),
     defaultValues: {
       tutorName: '',
-      // petName: '',
+      petName: '',
       // phone: '',
-      // description: '',
+      description: '',
     },
   });
 
@@ -54,12 +65,83 @@ export function AppointmentForm() {
             Preencha os dados do cliente para realizar o agendamento.
           </DialogDescription>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="tutorName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-label-medium-size">
+                    Nome do tutor
+                  </FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <User
+                        className="-translate-y-1/2 absolute top-1/2 left-3 transform text-content-brand"
+                        size={20}
+                      />
+                      <Input
+                        placeholder="Nome do tutor"
+                        className="pl-10"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="petName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-label-medium-size">
+                    Nome do pet
+                  </FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <Dog
+                        className="-translate-y-1/2 absolute top-1/2 left-3 transform text-content-brand"
+                        size={20}
+                      />
+                      <Input
+                        placeholder="Nome do pet"
+                        className="pl-10"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-label-medium-size">
+                    Descrição do serviço
+                  </FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Descrição do serviço"
+                      className="resize-none"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button variant="brand" type="submit">
+              Agendar
+            </Button>
+          </form>
+        </Form>
       </DialogContent>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <input {...form.register('tutorName')} />
-        </form>
-      </Form>
     </Dialog>
   );
 }
---
+++ src/components/ui/textarea.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+
+export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          'flex min-h-20 w-full rounded-md border border-border-primary bg-background-tertiary px-3 py-3 text-sm ring-offset-background placeholder:text-muted-foreground hover:border-border-secondary focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-border-brand focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50',
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Textarea.displayName = 'Textarea';
+
+export { Textarea };
