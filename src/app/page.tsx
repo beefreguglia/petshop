44026478bb266745d7eb76d188a30dab098046ef
Bernardo Freguglia <beefreguglia@gmail.com>
2025-12-11 05:44:17 -0300

    feature: remove unused component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
-type Props = {
-  title: string;
-};
-
-const Component = ({ title }: Props) => {
-  return (
-    <div>
-      <h2>Component {title}</h2>
-    </div>
-  );
-};
-
 export default function Home() {
   return (
     <div>
       <h2>Rocketseat</h2>
-
-      <Component title="title" />
     </div>
   );
 }
