5cc1f0bea63289b4c50c3fb0589947674f9259fb
Bernardo Freguglia <beefreguglia@gmail.com>
2025-12-11 16:58:01 -0300

    feature: create phone mask input

--- src/components/appoitment-form.tsx
+++ src/components/appoitment-form.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Dog, User } from 'lucide-react';
+import { Dog, Phone, User } from 'lucide-react';
 import { useForm } from 'react-hook-form';
+import { IMaskInput } from 'react-imask';
 import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
@@ -28,7 +29,7 @@ import { Textarea } from '@/components/ui/textarea';
 const appointmentFormSchema = z.object({
   tutorName: z.string().min(3, 'O nome do tutor é obrigatório'),
   petName: z.string().min(3, 'O nome do pet é obrigatório'),
-  // phone: z.string().min(11, 'O número de telefone é obrigatório'),
+  phone: z.string().min(11, 'O número de telefone é obrigatório'),
   description: z.string().min(3, 'A descrição é obrigatória'),
 });
 
@@ -40,7 +41,7 @@ export function AppointmentForm() {
     defaultValues: {
       tutorName: '',
       petName: '',
-      // phone: '',
+      phone: '',
       description: '',
     },
   });
@@ -92,6 +93,7 @@ export function AppointmentForm() {
                 </FormItem>
               )}
             />
+
             <FormField
               control={form.control}
               name="petName"
@@ -117,6 +119,34 @@ export function AppointmentForm() {
                 </FormItem>
               )}
             />
+
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-label-medium-size">
+                    Telefone
+                  </FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <Phone
+                        className="-translate-y-1/2 absolute top-1/2 left-3 transform text-content-brand"
+                        size={20}
+                      />
+                      <IMaskInput
+                        className="flex h-12 w-full rounded-md border border-border-primary bg-background-tertiary px-3 py-2 pl-10 text-content-primary text-sm ring-offset-background file:border-0 file:bg-transparent file:font-medium file:text-sm placeholder:text-content-secondary hover:border-border-secondary focus:border-border-brand focus-visible:border-border-brand focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-border-brand focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20"
+                        placeholder="(99) 99999-9999"
+                        mask="(00) 00000-0000"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
             <FormField
               control={form.control}
               name="description"
