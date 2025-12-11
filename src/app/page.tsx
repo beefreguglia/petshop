9fe0846804d68057fec9f7e82113f97481d00bf6
Bernardo Freguglia <beefreguglia@gmail.com>
2025-12-11 05:36:55 -0300

    feature: create lefthook configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+type Props = {
+  title: string;
+};
+
+const Component = ({ title }: Props) => {
+  return (
+    <div>
+      <h2>Component {title}</h2>
+    </div>
+  );
+};
+
 export default function Home() {
-    return (
-        <div>
-            <h2>Rocketseat</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Rocketseat</h2>
+
+      <Component title={2} />
+    </div>
+  );
 }
