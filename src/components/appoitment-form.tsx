b76e47123563b4a39b6e61bb6530c91a714b6fa2
Bernardo Freguglia <beefreguglia@gmail.com>
2025-12-11 17:55:54 -0300

    feature: create time component

--- src/components/appoitment-form.tsx
+++ src/components/appoitment-form.tsx
@@ -1,8 +1,16 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { format, startOfToday } from 'date-fns';
-import { CalendarIcon, ChevronDownIcon, Dog, Phone, User } from 'lucide-react';
+import { format, setHours, setMinutes, startOfToday } from 'date-fns';
+import {
+  CalendarIcon,
+  ChevronDownIcon,
+  Clock,
+  Dog,
+  Loader2,
+  Phone,
+  User,
+} from 'lucide-react';
 import { useForm } from 'react-hook-form';
 import { IMaskInput } from 'react-imask';
 import { z } from 'zod';
@@ -31,18 +39,40 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
 import { cn } from '@/lib/utils';
+import { TIME_OPTIONS } from '@/utils/time-options';
 
-const appointmentFormSchema = z.object({
-  tutorName: z.string().min(3, 'O nome do tutor é obrigatório'),
-  petName: z.string().min(3, 'O nome do pet é obrigatório'),
-  phone: z.string().min(11, 'O número de telefone é obrigatório'),
-  description: z.string().min(3, 'A descrição é obrigatória'),
-  scheduleAt: z
-    .date({ error: 'A data é obrigatória' })
-    .min(startOfToday(), { message: 'A data não pode ser no passado' }),
-});
+const appointmentFormSchema = z
+  .object({
+    tutorName: z.string().min(3, 'O nome do tutor é obrigatório'),
+    petName: z.string().min(3, 'O nome do pet é obrigatório'),
+    phone: z.string().min(11, 'O número de telefone é obrigatório'),
+    description: z.string().min(3, 'A descrição é obrigatória'),
+    scheduleAt: z
+      .date({ error: 'A data é obrigatória' })
+      .min(startOfToday(), { message: 'A data não pode ser no passado' }),
+    time: z.string().min(1, 'A hora é obrigatória'),
+  })
+  .refine(
+    (data) => {
+      const [hour, minute] = data.time.split(':');
+      const scheduleDateTime = setMinutes(
+        setHours(data.scheduleAt, Number(hour)),
+        Number(minute)
+      );
+
+      return scheduleDateTime > new Date();
+    },
+    { path: ['time'], error: 'O horario não pode ser no passado' }
+  );
 
 type AppointmentFormValues = z.infer<typeof appointmentFormSchema>;
 
@@ -55,6 +85,7 @@ export function AppointmentForm() {
       phone: '',
       description: '',
       scheduleAt: undefined,
+      time: '',
     },
   });
 
@@ -179,57 +210,101 @@ export function AppointmentForm() {
               )}
             />
 
-            <FormField
-              control={form.control}
-              name="scheduleAt"
-              render={({ field }) => (
-                <FormItem className="flex flex-col">
-                  <FormLabel className="text-content-primary text-label-medium-size">
-                    Data
-                  </FormLabel>
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <FormControl>
-                        <Button
-                          variant="outline"
-                          className={cn(
-                            'w-full justify-between border-border-primary bg-background-tertiary text-left font-normal text-content-primary hover:border-border-secondary hover:bg-background-tertiary hover:text-content-primary focus:border-border-brand focus-visible:border-border-brand focus-visible:ring-1 focus-visible:ring-border-brand focus-visible:ring-offset-0',
-                            !field.value && 'text-content-secondary'
-                          )}
-                        >
-                          <div className="flex items-center gap-2">
-                            <CalendarIcon
-                              className="text-content-brand"
-                              size={20}
-                            />
-                            {field.value ? (
-                              format(field.value, 'dd/MM/yyyy')
-                            ) : (
-                              <span>Selecione uma data</span>
+            <div className="space-y-4 md:grid md:grid-cols-2 md:gap-4 md:space-y-0">
+              <FormField
+                control={form.control}
+                name="scheduleAt"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel className="text-content-primary text-label-medium-size">
+                      Data
+                    </FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant="outline"
+                            className={cn(
+                              'w-full justify-between border-border-primary bg-background-tertiary text-left font-normal text-content-primary hover:border-border-secondary hover:bg-background-tertiary hover:text-content-primary focus:border-border-brand focus-visible:border-border-brand focus-visible:ring-1 focus-visible:ring-border-brand focus-visible:ring-offset-0',
+                              !field.value && 'text-content-secondary'
                             )}
+                          >
+                            <div className="flex items-center gap-2">
+                              <CalendarIcon
+                                className="text-content-brand"
+                                size={20}
+                              />
+                              {field.value ? (
+                                format(field.value, 'dd/MM/yyyy')
+                              ) : (
+                                <span>Selecione uma data</span>
+                              )}
+                            </div>
+                            <ChevronDownIcon className="size-4 opacity-50" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          mode="single"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          disabled={(date) => date < startOfToday()}
+                        />
+                      </PopoverContent>
+                    </Popover>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="time"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-content-primary text-label-medium-size">
+                      Hora
+                    </FormLabel>
+                    <FormControl>
+                      <Select
+                        onValueChange={field.onChange}
+                        value={field.value}
+                      >
+                        <SelectTrigger>
+                          <div className="flex items-center gap-2">
+                            <Clock className="size-4 text-content-brand" />
+                            <SelectValue placeholder="--:-- --" />
                           </div>
-                          <ChevronDownIcon className="size-4 opacity-50" />
-                        </Button>
-                      </FormControl>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0" align="start">
-                      <Calendar
-                        mode="single"
-                        selected={field.value}
-                        onSelect={field.onChange}
-                        disabled={(date) => date < startOfToday()}
-                      />
-                    </PopoverContent>
-                  </Popover>
-
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <Button variant="brand" type="submit">
-              Agendar
-            </Button>
+                        </SelectTrigger>
+                        <SelectContent>
+                          {TIME_OPTIONS.map((time) => (
+                            <SelectItem key={time} value={time}>
+                              {time}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex justify-end">
+              <Button
+                variant="brand"
+                type="submit"
+                disabled={form.formState.isSubmitting}
+              >
+                {form.formState.isSubmitting ? (
+                  <Loader2 className="mx-2 size-4 animate-spin text-content-primary" />
+                ) : (
+                  'Agendar'
+                )}
+              </Button>
+            </div>
           </form>
         </Form>
       </DialogContent>
